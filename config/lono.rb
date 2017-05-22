384f82cd785087575cba8241473e23b88be1d9bf
Tung Nguyen <tongueroo@gmail.com>
2017-05-22 10:11:21 -0700

    add lono version of the single instance and instance_and_route53 templates

---
+++ config/lono.rb
@@ -0,0 +1,10 @@
+template "single_instance.yml" do
+  source "instance.yml.erb"
+end
+
+template "instance_and_route53.yml" do
+  source "instance.yml.erb"
+  variables(
+    route53: true
+  )
+end
